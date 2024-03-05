761a8150154bbc47c2bfce650cb7f2d9183c1a14
Jakub Ondruska <jakubo.oondruska@gmail.com>
2024-03-05 20:33:27 +0100

    My first web ordering application, let's go

---
+++ src/main/java/com/example/bistro_springboot/BistroSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bistro_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BistroSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BistroSpringBootApplication.class, args);
+	}
+
+}
